2baca0e3dbaf539bc8ba14b447f225b8fd6bea19
karrambaby <karrambaby2106@gmail.com>
2022-11-22 20:20:44 +0200

    Updates

---
+++ precache-manifest.7c5bf78c5d6705dc86c503c90249ef93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b219e5c5c195955bc4fd",
+    "url": "/aboba/static/css/main.c2107f72.chunk.css"
+  },
+  {
+    "revision": "b219e5c5c195955bc4fd",
+    "url": "/aboba/static/js/main.b219e5c5.chunk.js"
+  },
+  {
+    "revision": "c70bc214e53f5b7f186a",
+    "url": "/aboba/static/css/1.fe40f168.chunk.css"
+  },
+  {
+    "revision": "c70bc214e53f5b7f186a",
+    "url": "/aboba/static/js/1.c70bc214.chunk.js"
+  },
+  {
+    "revision": "d1215dce1b4f2e65d716",
+    "url": "/aboba/static/js/runtime~main.d1215dce.js"
+  },
+  {
+    "revision": "4c8f1940ca0734d38c3fad5bf9b8ee32",
+    "url": "/aboba/static/media/1.4c8f1940.jpg"
+  },
+  {
+    "revision": "c93762b141f748aa2b2ccb9fd3757923",
+    "url": "/aboba/index.html"
+  }
+];
